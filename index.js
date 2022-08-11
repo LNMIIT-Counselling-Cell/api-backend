5bb6d88da4bd5ab934aae91cb6d81136a3e8b2ed
Sriraj <behera.sriraj2911@gmail.com>
2022-08-12 01:22:41 +0530

    register models and routes

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const db = require('./database')
 app.use(express.json());
 
 require('./models/user');
+require('./models/outpass');
 db.model("User");
+db.model("Outpass")
 app.use(require('./routes/auth'));
+app.use(require('./routes/outpass'));
+app.use(require('./routes/admin'));
 
 app.listen(PORT, () => {
   console.log(`The Server is running on port: ${PORT}`);
