bbcb83773fae267579ed7895f60da892159e53e9
sourabhjo7 <sourabhjoshi9549408165@gmail.com>
2022-08-25 23:38:08 +0530

    git ignore updated

--- models/outpass.js
+++ models/outpass.js
@@ -40,12 +40,13 @@ const outpassSchema = new mongoose.Schema({
     default: "Pending",
     required: true
   }
+
 }, {
   // will automatically give a timestamp of createdAt and updatedAt
   timestamps: {
     createdAt: 'created_at',
     updatedAt: 'updated_at'
   }
-})
+} )
 
 mongoose.model("Outpass", outpassSchema);
